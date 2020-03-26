50ea73f501a5d39cbba062d92249ddb97a1b5b02
János Csikós <csikosjanos@gmail.com>
2020-03-26 22:18:38 +0000

    Uj konyv form elkeszites

--- client/apps/konyvek/src/app/app.module.ts
+++ client/apps/konyvek/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { KonyvlistaComponent } from './konyvlista/konyvlista.component';
 import { UjKonyvComponent } from './uj-konyv/uj-konyv.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, KonyvlistaComponent, UjKonyvComponent],
@@ -13,6 +14,7 @@ import { UjKonyvComponent } from './uj-konyv/uj-konyv.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/apps/konyvek/src/app/uj-konyv/uj-konyv.component.ts
+++ client/apps/konyvek/src/app/uj-konyv/uj-konyv.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'client-uj-konyv',
   templateUrl: './uj-konyv.component.html',
   styleUrls: ['./uj-konyv.component.css']
 })
-export class UjKonyvComponent implements OnInit {
+export class UjKonyvComponent {
+
+  konyvForm = new FormGroup({
+    azon: new FormControl(''),
+    szerzo: new FormControl(''),
+    cim: new FormControl(''),
+    kiadasev: new FormControl(''),
+    mennyiseg: new FormControl(''),
+    ar: new FormControl(''),
+    kolcsonozheto: new FormControl(''),
+  });
 
   constructor() { }
 
-  ngOnInit(): void {
+  onSubmit() {
+    console.log(this.konyvForm.value);
   }
-
 }
