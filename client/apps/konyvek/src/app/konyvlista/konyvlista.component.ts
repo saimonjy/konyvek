9017208671c3841f118c77890777c09b3223d1e2
János Csikós <csikosjanos@gmail.com>
2020-03-26 21:17:50 +0000

    konyvek listajat atrakjuk a konyvlista komponensbe az app komponensbol

--- client/apps/konyvek/src/app/konyvlista/konyvlista.component.ts
+++ client/apps/konyvek/src/app/konyvlista/konyvlista.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// interface Konyv {
+//   azon: string;
+//   ...
+// }
 
 @Component({
   selector: 'client-konyvlista',
@@ -6,8 +12,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./konyvlista.component.css']
 })
 export class KonyvlistaComponent implements OnInit {
+  // konyvek:Observable<Konyv[]> = this.http.get('http://192.168.64.2/blabla/server/konyvlista.php');
+  konyvek = this.http.get('http://192.168.64.2/blabla/server/konyvlista.php');
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
   }
