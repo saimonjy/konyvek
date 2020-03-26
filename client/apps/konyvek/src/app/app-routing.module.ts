8e71ae0e8af24b59c04610db20b4be1ca4292278
János Csikós <csikosjanos@gmail.com>
2020-03-26 21:23:27 +0000

    menu es routing a homepage-nek

--- client/apps/konyvek/src/app/app-routing.module.ts
+++ client/apps/konyvek/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { KonyvlistaComponent } from './konyvlista/konyvlista.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: KonyvlistaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
